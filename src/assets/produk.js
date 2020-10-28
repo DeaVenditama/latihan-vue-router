abd238b20440e4d7f94524aa266b3516ba477480
DeaVenditama <deavenditama@gmail.com>
2020-10-28 13:27:03 +0700

    first push

---
+++ src/assets/produk.js
@@ -0,0 +1,19 @@
+export const produk={
+  "produk" : [
+    {
+      "id" : 1,
+      "nama" : "Handphone",
+      "harga" : "Rp 1.000.000,00"
+    },
+    {
+      "id" : 2,
+      "nama" : "TV",
+      "harga" : "Rp 2.000.000,00"
+    },
+    {
+      "id" : 3,
+      "nama" : "Laptop",
+      "harga" : "Rp 10.000.000,00"
+    },
+  ]
+}
